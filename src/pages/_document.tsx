55656cd69a2a6a065aeac8f9b05e1e9572c0c06c
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2022-03-05 17:15:02 -0300

    login with github

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,8 @@ export default class MyDocument extends Document {
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@400;700&family=Roboto:wght@900&display=swap" rel="stylesheet" />
-          
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
