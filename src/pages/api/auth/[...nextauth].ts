fd0faec399f494a21e8a71455cd22b6def9a2f23
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2022-03-13 22:25:04 -0300

    sign in and insert in databse fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,17 @@
-import NextAuth from "next-auth";
+import { query as q } from "faunadb";
+import NextAuth, { Account, Profile, User } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+    verificationRequest?: boolean;
+  };
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +22,26 @@ export default NextAuth({
       authorization: { params: { scope: "read:user" } },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email))),
+          ),
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -4,6 +4,5 @@ export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
     name: "ignews",
-    version,
   },
 });
