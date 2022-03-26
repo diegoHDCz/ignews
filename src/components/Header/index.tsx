ba985bddd98ce69c31f3ac7247e44403e75b39c3
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2022-03-26 16:35:17 -0300

    routing and styles button routes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss'
-import Link from 'next/link'
+import styles from './styles.module.scss';
+
 
 
 
 export function Header () {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
 
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active} >Home</a>
-          </Link>
-          <Link href="/posts">
-            <a >Posts</a>
-          </Link>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
